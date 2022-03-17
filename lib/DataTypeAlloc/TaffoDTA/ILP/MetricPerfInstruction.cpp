TAFFO-org/TAFFO

commit cc493e91d6a73fef79f7d6be02ce218b48a11e50

DTA ILP: Ensure no variable name clashing happens

lib/DataTypeAlloc/TaffoDTA/ILP/MetricPerfInstruction.cpp
@@ -588,8 +588,7 @@ void MetricPerf::handleCastInstruction(Instruction *instruction, shared_ptr<Valu
 
       // Do not save! As here we have a pointer!
       shared_ptr<OptimizerScalarInfo> variable = allocateNewVariableForValue(instruction, fptype, fieldInfo->IRange,
-                                                                             fieldInfo->IError,
-                                                                             instruction->getFunction()->getName().str(), false);
+                                                                             fieldInfo->IError);
 
       auto met = make_shared<OptimizerPointerInfo>(variable);
 
@@ -640,8 +639,7 @@ void MetricPerf::handleCastInstruction(Instruction *instruction, shared_ptr<Valu
 
 
     shared_ptr<OptimizerScalarInfo> variable = allocateNewVariableForValue(instruction, fptype, fieldInfo->IRange,
-                                                                           fieldInfo->IError,
-                                                                           instruction->getFunction()->getName().str());
+                                                                           fieldInfo->IError);
 
     // Limiting the ENOB as coming from an integer we can have an error at min of 1
     // Look that here we have the original program, so these instruction are not related to fixed point implementation!
@@ -897,8 +895,7 @@ void MetricPerf::handlePhi(Instruction *instruction, shared_ptr<ValueInfo> value
 
   // Allocating variable for result
   shared_ptr<OptimizerScalarInfo> variable = allocateNewVariableForValue(instruction, fptype, fieldInfo->IRange,
-                                                                         fieldInfo->IError,
-                                                                         instruction->getFunction()->getName().str());
+                                                                         fieldInfo->IError);
   auto constraint = vector<pair<string, double>>();
   constraint.clear();
 
@@ -915,7 +912,7 @@ void MetricPerf::handlePhi(Instruction *instruction, shared_ptr<ValueInfo> value
       }
     }
 
-    string enob_selection = getEnobActivationVariable(instruction, index);
+    string enob_selection = getEnobActivationVariable(phi_n, index);
     getModel().createVariable(enob_selection, 0, 1);
     constraint.push_back(make_pair(enob_selection, 1.0));
   }

lib/DataTypeAlloc/TaffoDTA/CMakeLists.txt
@@ -26,6 +26,8 @@ if (TAFFO_BUILD_ILP_DTA)
     ILP/Model.h
     ILP/CPUCosts.h
     ILP/LoopAnalyzerUtil.h
+    ILP/Utils.h
+    ILP/Utils.cpp
     )
 endif ()
 

lib/DataTypeAlloc/TaffoDTA/ILP/MetricBase.cpp
@@ -87,7 +87,6 @@ shared_ptr<tuner::OptimizerInfo> MetricBase::processConstant(Constant *constant)
 
       Range rangeInfo(min, max);
 
-
       FixedPointTypeGenError fpgerr;
       FPType fpInfo = fixedPointTypeFromRange(rangeInfo, &fpgerr, TotalBits, FracThreshold, 64, TotalBits);
       if (fpgerr != FixedPointTypeGenError::NoError) {
@@ -96,10 +95,9 @@ shared_ptr<tuner::OptimizerInfo> MetricBase::processConstant(Constant *constant)
         return nullptr;
       }
 
-      string fname = "ConstantValue";
       // ENOB should not be considered for constant.... It is a constant and will be converted as best as possible
       // WE DO NOT SAVE CONSTANTS INFO!
-      auto info = allocateNewVariableForValue(constantFP, make_shared<FPType>(fpInfo), make_shared<Range>(rangeInfo), nullptr, fname, false, "", false);
+      auto info = allocateNewVariableForValue(constantFP, make_shared<FPType>(fpInfo), make_shared<Range>(rangeInfo), nullptr, false, "", false);
       info->setReferToConstant(true);
       return info;
     }
@@ -187,11 +185,6 @@ shared_ptr<OptimizerStructInfo> MetricBase::loadStructInfo(Value *glob, shared_p
 {
   shared_ptr<OptimizerStructInfo> optInfo = make_shared<OptimizerStructInfo>(pInfo->size());
 
-  string function = "";
-  if (auto instr = dyn_cast_or_null<Instruction>(glob)) {
-    function = instr->getFunction()->getName().str();
-  }
-
   int i = 0;
   for (auto it = pInfo->begin(); it != pInfo->end(); it++) {
     if (auto structInfo = dynamic_ptr_cast_or_null<StructInfo>(*it)) {
@@ -202,7 +195,7 @@ shared_ptr<OptimizerStructInfo> MetricBase::loadStructInfo(Value *glob, shared_p
         LLVM_DEBUG(dbgs() << "No fixed point info associated. Bailing out.\n");
 
       } else {
-        auto info = allocateNewVariableForValue(glob, fptype, ii->IRange, ii->IError, function, false,
+        auto info = allocateNewVariableForValue(glob, fptype, ii->IRange, ii->IError, false,
                                                 name + "_" + to_string(i));
         optInfo->setField(i, info);
       }
@@ -383,8 +376,7 @@ MetricBase::handleUnaryOpCommon(Instruction *instr, Value *op1, bool forceFixEqu
 
   // Obviously the type should be sufficient to contain the result
   shared_ptr<OptimizerScalarInfo> result = allocateNewVariableForValue(instr, fptype, inputInfo->IRange,
-                                                                       inputInfo->IError,
-                                                                       instr->getFunction()->getName().str());
+                                                                       inputInfo->IError);
 
   opt->insertTypeEqualityConstraint(varCast1, result, forceFixEquality);
 
@@ -423,8 +415,7 @@ MetricBase::handleBinOpCommon(Instruction *instr, Value *op1, Value *op2, bool f
 
   // Obviously the type should be sufficient to contain the result
   shared_ptr<OptimizerScalarInfo> result = allocateNewVariableForValue(instr, fptype, inputInfo->IRange,
-                                                                       inputInfo->IError,
-                                                                       instr->getFunction()->getName().str());
+                                                                       inputInfo->IError);
 
   opt->insertTypeEqualityConstraint(varCast1, varCast2, forceFixEquality);
   opt->insertTypeEqualityConstraint(varCast1, result, forceFixEquality);
@@ -538,8 +529,7 @@ void MetricBase::handleCall(Instruction *instruction, shared_ptr<ValueInfo> valu
                    dbgs() << "Info: " << inputInfo->toString() << "\n";);
         shared_ptr<OptimizerScalarInfo> result = allocateNewVariableForValue(instruction, fptype,
                                                                              inputInfo->IRange,
-                                                                             inputInfo->IError,
-                                                                             instruction->getFunction()->getNameOrAsOperand());
+                                                                             inputInfo->IError);
         retInfo = result;
         LLVM_DEBUG(dbgs() << "Allocated variable for returns.\n";);
       } else {
@@ -757,7 +747,6 @@ void MetricBase::handleUnknownFunction(Instruction *instruction, shared_ptr<Valu
         shared_ptr<OptimizerScalarInfo> result = allocateNewVariableForValue(instruction, fptype,
                                                                              inputInfo->IRange,
                                                                              inputInfo->IError,
-                                                                             call_i->getFunction()->getName().str(),
                                                                              true,
                                                                              "",
                                                                              true,
@@ -884,7 +873,6 @@ void MetricBase::handleAlloca(Instruction *instruction, shared_ptr<ValueInfo> va
         return;
       }
       auto info = allocateNewVariableForValue(alloca, fptype, fieldInfo->IRange, fieldInfo->IError,
-                                              alloca->getFunction()->getName().str(),
                                               false);
       saveInfoForValue(alloca, make_shared<OptimizerPointerInfo>(info));
     } else if (valueInfo->metadata->getKind() == MDInfo::K_Struct) {

lib/DataTypeAlloc/TaffoDTA/ILP/MetricBase.h
@@ -78,7 +78,7 @@ public:
   virtual shared_ptr<tuner::OptimizerScalarInfo> allocateNewVariableForValue(
       llvm::Value *value, shared_ptr<mdutils::FPType> fpInfo,
       shared_ptr<mdutils::Range> rangeInfo,
-      shared_ptr<double> suggestedMinError, string functionName,
+      shared_ptr<double> suggestedMinError,
       bool insertInList = true, string nameAppendix = "",
       bool insertENOBinMin = true, bool respectFloatingPointConstraint = true) = 0;
   virtual void saveInfoForValue(llvm::Value *value,
@@ -178,7 +178,7 @@ public:
   shared_ptr<tuner::OptimizerScalarInfo> allocateNewVariableForValue(
       llvm::Value *value, shared_ptr<mdutils::FPType> fpInfo,
       shared_ptr<mdutils::Range> rangeInfo,
-      shared_ptr<double> suggestedMinError, string functionName,
+      shared_ptr<double> suggestedMinError,
       bool insertInList = true, string nameAppendix = "",
       bool insertENOBinMin = true,
       bool respectFloatingPointConstraint = true) override;

lib/DataTypeAlloc/TaffoDTA/ILP/MetricPerf.cpp
@@ -10,22 +10,16 @@ using namespace mdutils;
 
 
 shared_ptr<tuner::OptimizerScalarInfo> MetricPerf::allocateNewVariableForValue(Value *value, shared_ptr<mdutils::FPType> fpInfo, shared_ptr<mdutils::Range> rangeInfo, shared_ptr<double> suggestedMinError,
-                                                                               string functionName, bool insertInList, string nameAppendix, bool insertENOBinMin, bool respectFloatingPointConstraint)
+                                                                               bool insertInList, string nameAppendix, bool insertENOBinMin, bool respectFloatingPointConstraint)
 {
   assert(!valueHasInfo(value) && "The value considered already have an info!");
 
   assert(fpInfo && "fpInfo should not be nullptr here!");
   assert(rangeInfo && "rangeInfo should not be nullptr here!");
 
-  if (!functionName.empty()) {
-    functionName = functionName.append("_");
-  }
-
-
   auto &model = getModel();
 
-
-  string varNameBase(string(functionName).append((std::string)value->getName()).append(nameAppendix));
+  string varNameBase = tuner::uniqueIDForValue(value).append(nameAppendix);
   std::replace(varNameBase.begin(), varNameBase.end(), '.', '_');
   string varName(varNameBase);
 
@@ -34,7 +28,6 @@ shared_ptr<tuner::OptimizerScalarInfo> MetricPerf::allocateNewVariableForValue(V
     varName = string(varNameBase).append("_").append(to_string(counter));
     counter++;
   }
-
 
   LLVM_DEBUG(llvm::dbgs() << "Allocating new variable, will have the following name: " << varName << "\n";);
 
@@ -732,16 +725,6 @@ std::string MetricPerf::getEnobActivationVariable(Value *value, int cardinal)
 
   assert(!valueName.empty() && "The value should have a name!!!");
 
-  string fname;
-  if (auto instr = dyn_cast_or_null<Instruction>(value)) {
-    fname = instr->getFunction()->getName().str();
-    std::replace(fname.begin(), fname.end(), '.', '_');
-  }
-
-  if (!fname.empty()) {
-    valueName = fname + "_" + valueName;
-  }
-
   string toreturn = valueName + "_enob_" + to_string(cardinal);
 
   return toreturn;
@@ -872,8 +855,7 @@ void MetricPerf::handleSelect(Instruction *instruction, shared_ptr<tuner::ValueI
 
   // Allocating variable for result
   shared_ptr<tuner::OptimizerScalarInfo> variable = allocateNewVariableForValue(instruction, fptype, fieldInfo->IRange,
-                                                                                fieldInfo->IError,
-                                                                                instruction->getFunction()->getName().str());
+                                                                                fieldInfo->IError);
   auto constraint = vector<pair<string, double>>();
   auto &model = getModel();
   constraint.clear();

lib/DataTypeAlloc/TaffoDTA/ILP/Optimizer.cpp
@@ -76,7 +76,7 @@ void Optimizer::handleGlobal(GlobalObject *glob, shared_ptr<ValueInfo> valueInfo
         LLVM_DEBUG(dbgs() << "No fixed point info associated. Bailing out.\n");
         return;
       }
-      auto optInfo = metric->allocateNewVariableForValue(glob, fptype, fieldInfo->IRange, fieldInfo->IError, "", false);
+      auto optInfo = metric->allocateNewVariableForValue(glob, fptype, fieldInfo->IRange, fieldInfo->IError, false);
       metric->saveInfoForValue(glob, make_shared<OptimizerPointerInfo>(optInfo));
     } else if (valueInfo->metadata->getKind() == MDInfo::K_Struct) {
       LLVM_DEBUG(dbgs() << " ^ This is a real structure\n");
@@ -123,7 +123,7 @@ void Optimizer::handleGlobal(GlobalObject *glob, shared_ptr<ValueInfo> valueInfo
         LLVM_DEBUG(dbgs() << "Has initializer and it is not a null value! Need more processing!\n");
       } else {
         LLVM_DEBUG(dbgs() << "No initializer, or null value!\n");
-        auto optInfo = metric->allocateNewVariableForValue(glob, fptype, fieldInfo->IRange, fieldInfo->IError, "", false);
+        auto optInfo = metric->allocateNewVariableForValue(glob, fptype, fieldInfo->IRange, fieldInfo->IError, false);
         // This is a pointer, so the reference to it is a pointer to a pointer yay
         metric->saveInfoForValue(glob, make_shared<OptimizerPointerInfo>(make_shared<OptimizerPointerInfo>(optInfo)));
       }
@@ -218,8 +218,7 @@ void Optimizer::handleCallFromRoot(Function *f)
       auto fptype = dynamic_ptr_cast_or_null<FPType>(inputInfo->IType);
       if (fptype) {
           LLVM_DEBUG(dbgs() << fptype->toString(););
-          shared_ptr<OptimizerScalarInfo> result = allocateNewVariableForValue(instruction, fptype, inputInfo->IRange,
-                                                                               instruction->getFunction()->getName());
+          shared_ptr<OptimizerScalarInfo> result = allocateNewVariableForValue(instruction, fptype, inputInfo->IRange);
           retInfo = result;
       } else {
           LLVM_DEBUG(dbgs() << "There was an input info but no fix point associated.\n";);

lib/DataTypeAlloc/TaffoDTA/ILP/Utils.cpp
@@ -1,0 +1,48 @@
+#include "TaffoDTA.h"
+#include "ILP/Utils.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Constant.h"
+#include "llvm/IR/Operator.h"
+#include "llvm/Support/Debug.h"
+
+using namespace llvm;
+
+std::string tuner::uniqueIDForValue(Value *value)
+{
+  std::string buf;
+
+  do {
+    raw_string_ostream stm(buf);
+
+    if (Instruction *inst = dyn_cast<Instruction>(value)) {
+      stm << "instr_" << inst->getFunction()->getName() << "_";
+    } else if (Argument *arg = dyn_cast<Argument>(value)) {
+      stm << "funarg_" << arg->getParent()->getName() << "_";
+    } else if (isa<Constant>(value) || isa<Operator>(value)) {
+      stm << "const_";
+    } else {
+      llvm_unreachable("found value type unsupported by the DTA");
+    }
+
+    if (value->hasName()) {
+      stm << value->getName().str();
+    } else {
+      value->printAsOperand(stm, false);
+    }
+    stm << '_';
+
+    // add the value's pointer for good measure
+    stm << std::to_string((intptr_t)value);
+  } while (0);
+
+  // sanitize
+  for (auto c = buf.begin(); c != buf.end(); c++) {
+    if (!(isalnum(*c) || *c == '_'))
+      *c = '_';
+  }
+
+  return buf;
+}
+

lib/DataTypeAlloc/TaffoDTA/ILP/Utils.h
@@ -1,0 +1,12 @@
+#pragma once
+
+#include "llvm/IR/Value.h"
+#include <string>
+
+namespace tuner {
+
+  /// Utility function that generates an unique string ID from a value
+  std::string uniqueIDForValue(llvm::Value *value);
+
+}
+

commit c048444f6e888c04c52a8027aea8838e33b85c67

DTA ILP: Fix a bug accidentally introduced in cc493e9 that caused the pass to crash on handling bitcast instructions.

lib/DataTypeAlloc/TaffoDTA/ILP/MetricPerfInstruction.cpp
@@ -606,7 +606,7 @@ void MetricPerf::handleCastInstruction(Instruction *instruction, shared_ptr<Valu
 
       // Do not save! As here we have a pointer!
       shared_ptr<OptimizerScalarInfo> variable = allocateNewVariableForValue(instruction, fptype, fieldInfo->IRange,
-                                                                             fieldInfo->IError);
+                                                                             fieldInfo->IError, false);
 
       auto met = make_shared<OptimizerPointerInfo>(variable);
 