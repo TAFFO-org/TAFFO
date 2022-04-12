713d6f2418223e07f437fde67dedd09fde6d8c38
denisovlev <lev.denisov@polimi.it>
2022-04-12 11:52:07 +0200

    add legacy pass manager to tracing passes

--- lib/DynamicAnalysis/Tracing/Tracing.cpp
+++ lib/DynamicAnalysis/Tracing/Tracing.cpp
@@ -37,3 +37,57 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
   return getInjectFuncCallPluginInfo();
 }
+
+//------------------------------------------------------------------------------
+// Legacy PM interface
+//------------------------------------------------------------------------------
+struct LegacyInjectFuncCall : public llvm::ModulePass {
+  static char ID;
+  LegacyInjectFuncCall() : ModulePass(ID) {}
+  bool runOnModule(llvm::Module &M) override {
+    bool Changed = Impl.runOnModule(M);
+    return Changed;
+  }
+
+  InjectFuncCall Impl;
+};
+
+struct LegacyNameVariables : public llvm::ModulePass {
+  static char ID;
+  LegacyNameVariables() : ModulePass(ID) {}
+  bool runOnModule(llvm::Module &M) override {
+    bool Changed = Impl.runOnModule(M);
+    return Changed;
+  }
+
+  NameVariables Impl;
+};
+
+struct LegacyReadTrace : public llvm::ModulePass {
+  static char ID;
+  LegacyReadTrace() : ModulePass(ID) {}
+  bool runOnModule(llvm::Module &M) override {
+    bool Changed = Impl.runOnModule(M);
+    return Changed;
+  }
+
+  ReadTrace Impl;
+};
+
+char LegacyInjectFuncCall::ID = 0;
+char LegacyNameVariables::ID = 0;
+char LegacyReadTrace::ID = 0;
+
+// Register the pass - required for (among others) opt
+static RegisterPass<LegacyInjectFuncCall>
+        X(/*PassArg=*/"taffo-inject-func-call", /*Name=*/"TAFFO Framework inject tracing",
+        /*CFGOnly=*/false, /*is_analysis=*/false);
+
+static RegisterPass<LegacyNameVariables>
+        Y(/*PassArg=*/"taffo-name-variables", /*Name=*/"TAFFO Framework assign unique names to registers",
+        /*CFGOnly=*/false, /*is_analysis=*/false);
+
+static RegisterPass<LegacyReadTrace>
+        Z(/*PassArg=*/"taffo-read-trace", /*Name=*/"TAFFO Framework read range information from trace files",
+        /*CFGOnly=*/false, /*is_analysis=*/false);
+
