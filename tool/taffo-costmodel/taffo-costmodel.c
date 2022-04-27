64ed6e83ed0043a53fdb33a93a1f6162cb4bc4ec
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2022-04-27 19:35:24 +0200

    taffo-costmodel: Simplify codebase.

--- tool/taffo-costmodel/taffo-costmodel.c
+++ tool/taffo-costmodel/taffo-costmodel.c
@@ -16,14 +16,10 @@
  * @author Nicolas Limare <nicolas@limare.net>
  */
 
-#include "xutils.h"
 
 #define USE_TIMING
-
 #include "config.h"
 #include "timing.h"
-#include "collector.h"
-
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
@@ -37,21 +33,98 @@ typedef int64_t int64;
 typedef float flt32;
 typedef double flt64;
 
-#ifdef NOFLT80
-#define RUNFLT80 0
-typedef float flt80;
-#else
-#define RUNFLT80 1
-typedef __float80 flt80;
-#endif
 
-#ifdef NOFLT128
-#define RUNFLT128 0
-typedef float flt128;
-#else
-#define RUNFLT128 1
-typedef __float128 flt128;
-#endif
+enum e_ops {
+  OP_ADD,
+  OP_SUB,
+  OP_MUL,
+  OP_DIV,
+  OP_SHIFT,
+  OP_REM,
+  N_OP
+};
+const char *ops_names[N_OP] = {
+  "ADD",
+  "SUB",
+  "MUL",
+  "DIV",
+  "SHIFT",
+  "REM",
+};
+
+enum e_types {
+  T_FIX,
+  T_FLOAT,
+  T_DOUBLE,
+  N_T
+};
+const char *types_names[N_T] = {
+  "FIX",
+  "FLOAT",
+  "DOUBLE"
+};
+
+double op_times[N_OP][N_T];
+double cast_times[N_OP][N_OP];
+
+/*
+enum collecting {
+  ADD_FIX = 0,
+  ADD_FLOAT,
+  ADD_DOUBLE,
+  SUB_FIX,
+  SUB_FLOAT,
+  SUB_DOUBLE,
+  MUL_FIX,
+  MUL_FLOAT,
+  MUL_DOUBLE,
+  DIV_FIX,
+  DIV_FLOAT,
+  DIV_DOUBLE,
+  REM_FIX,
+  REM_FLOAT,
+  REM_DOUBLE,
+  LOG_FIX,
+  LOG_FLOAT,
+  LOG_DOUBLE,
+  CAST_FIX_FLOAT,
+  CAST_FIX_DOUBLE,
+  CAST_FLOAT_FIX,
+  CAST_FLOAT_DOUBLE,
+  CAST_DOUBLE_FIX,
+  CAST_DOUBLE_FLOAT,
+  CAST_FIX_FIX,
+  COLLECTION_SIZE
+};
+
+const char * coll[] = {
+  "ADD_FIX",
+  "ADD_FLOAT",
+  "ADD_DOUBLE",
+  "SUB_FIX",
+  "SUB_FLOAT",
+  "SUB_DOUBLE",
+  "MUL_FIX",
+  "MUL_FLOAT",
+  "MUL_DOUBLE",
+  "DIV_FIX",
+  "DIV_FLOAT",
+  "DIV_DOUBLE",
+  "REM_FIX",
+  "REM_FLOAT",
+  "REM_DOUBLE",
+  "CAST_FIX_FLOAT",
+  "CAST_FIX_DOUBLE",
+  "CAST_FLOAT_FIX",
+  "CAST_FLOAT_DOUBLE",
+  "CAST_DOUBLE_FIX",
+  "CAST_DOUBLE_FLOAT",
+  "CAST_FIX_FIX",
+  "ERRRRRRRRRRORRRRRRRRRRRR"
+};
+
+double times[COLLECTION_SIZE];
+*/
 
 /**
  * floating-point comparison, for qsort()
@@ -70,11 +143,11 @@ static int cmpf(const void *a, const void *b) {
     volatile TYPE * c = (TYPE *) _c;                \
     const size_t nbops = memsize / sizeof(TYPE);            \
     for (int n = 0; n < nbrun; n++) {                \
-        TIMING_CPUCLOCK_START(0);                    \
-        for (size_t i = 0; i < nbops; i++)                \
+      TIMING_CPUCLOCK_START(0);                    \
+      for (size_t i = 0; i < nbops; i++)                \
         OP;                            \
-        TIMING_CPUCLOCK_TOGGLE(0);                    \
-        cpuclock[n] = TIMING_CPUCLOCK_S(0);                \
+      TIMING_CPUCLOCK_TOGGLE(0);                    \
+      cpuclock[n] = TIMING_CPUCLOCK_S(0);                \
     }                                \
     qsort(cpuclock, nbrun, sizeof(float), &cmpf);            \
     T = nbops / 1E6 / cpuclock[nbrun/2];                \
@@ -82,45 +155,42 @@ static int cmpf(const void *a, const void *b) {
     (TYPE *)a;\
     (TYPE *)b;\
     (TYPE *)c;\
-    }
+  }
 
 /* multi int type macro */
-#define ITIME(OP) {                        \
-    t1=0; t2=0; t3=0; t4=0;                    \
-    TIME(OP, int8, t1);                    \
-    TIME(OP, int16, t2);                    \
-    TIME(OP, int32, t3);                    \
-    TIME(OP, int64, t4);                    \
-    fprintf(stderr, "'%-20s', %16.10f, %16.10f, %16.10f, %16.10f\n",    \
-           #OP, t1, t2, t3, t4);                \
-    fflush(stderr);\
-    }
-
+#define ITIME(OP_ID, OP) do { \
+    t1=0; t2=0; t3=0; t4=0; \
+    TIME(OP, int32, t3); \
+    op_times[OP_ID][T_FIX] = t3; \
+    fprintf(stderr, "'%-20s', %16.10f, %16.10f, %16.10f, %16.10f\n", \
+            #OP, t1, t2, t3, t4); \
+    fflush(stderr); \
+  } while (0);
 
 /* multi flt type macro */
-#define FTIME(OP) {                        \
-    t1=0; t2=0; t3=0; t4=0;                \
-    TIME(OP, flt32, t1);                    \
-    TIME(OP, flt64, t2);                    \
-    if (RUNFLT80) TIME(OP, flt80, t3);            \
-    if (RUNFLT128) TIME(OP, flt128, t4);            \
-    fprintf(stderr, "'%-20s', %16.10f, %16.10f, %16.10f, %16.10f\n",    \
-           #OP, t1, t2, t3, t4);            \
-    fflush(stderr);\
-    }
+#define FTIME(OP_ID, OP) do { \
+    t1=0; t2=0; t3=0; t4=0; \
+    TIME(OP, flt32, t1); \
+    TIME(OP, flt64, t2); \
+    op_times[OP_ID][T_FLOAT] = t1; \
+    op_times[OP_ID][T_DOUBLE] = t2; \
+    fprintf(stderr, "'%-20s', %16.10f, %16.10f, %16.10f, %16.10f\n", \
+            #OP, t1, t2, t3, t4); \
+    fflush(stderr); \
+  } while (0);
 
-#define max(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define CTIME(STYPE) {                        \
-    t1=0; t2=0; t3=0; t4=0; t5=0;                \
-    CONVTIME(STYPE, flt32, t1);                    \
-    CONVTIME(STYPE, flt64, t2);                    \
-    CONVTIME(STYPE, int32, t3);            \
-    if (RUNFLT80) CONVTIME(STYPE, flt80, t4);            \
-    if (RUNFLT128) CONVTIME(STYPE, flt128, t5);            \
-    fprintf(stderr, "'%-20s', %16.10f, %16.10f, %16.10f, %16.10f, %16.10f\n",    \
-           "Cast from " #STYPE, t1, t2, t3, t4, t5);            \
-    fflush(stderr);\
-    }
+/* multi flt function type macro */
+#define F_FUN_TIME(OP_ID, DEST, FUN_F, FUN_D, PARM) do { \
+    t1=0; t2=0; t3=0; t4=0; \
+    TIME(DEST = FUN_F PARM, flt32, t1); \
+    TIME(DEST = FUN_D PARM, flt64, t2); \
+    op_times[OP_ID][T_FLOAT] = t1; \
+    op_times[OP_ID][T_DOUBLE] = t2; \
+    fprintf(stderr, "'%-20s', %16.10f, %16.10f, %16.10f, %16.10f\n", \
+           #DEST " = " #FUN_F #PARM, t1, t2, t3, t4); \
+    fflush(stderr); \
+  } while (0);
+
 
 /* single type macro */
 #define CONVTIME(STYPE, DTYPE, T) {                        \
@@ -129,16 +199,30 @@ static int cmpf(const void *a, const void *b) {
     volatile DTYPE * b = (DTYPE *) _b;                \
     const size_t nbops = memsize / max(sizeof(STYPE), sizeof(DTYPE));            \
     for (int n = 0; n < nbrun; n++) {                \
-        TIMING_CPUCLOCK_START(0);                    \
-        for (size_t i = 0; i < nbops; i++)                \
+      TIMING_CPUCLOCK_START(0);                    \
+      for (size_t i = 0; i < nbops; i++)                \
         b[i] = (DTYPE) a[i];                            \
-        TIMING_CPUCLOCK_TOGGLE(0);                    \
-        cpuclock[n] = TIMING_CPUCLOCK_S(0);                \
+      TIMING_CPUCLOCK_TOGGLE(0);                    \
+      cpuclock[n] = TIMING_CPUCLOCK_S(0);                \
     }                                \
     qsort(cpuclock, nbrun, sizeof(float), &cmpf);            \
     T = nbops / 1E6 / cpuclock[nbrun/2];                \
     T = 1000.0/T; \
-    }
+  }
+
+#define max(X, Y) (((X) > (Y)) ? (X) : (Y))
+#define CTIME(STYPE_ID, STYPE) do { \
+    t1=0; t2=0; t3=0; t4=0; t5=0; \
+    CONVTIME(STYPE, flt32, t1); \
+    CONVTIME(STYPE, flt64, t2); \
+    CONVTIME(STYPE, int32, t3); \
+    cast_times[STYPE_ID][T_FLOAT] = t1; \
+    cast_times[STYPE_ID][T_DOUBLE] = t2; \
+    cast_times[STYPE_ID][T_FIX] = t3; \
+    fprintf(stderr, "'%-20s', %16.10f, %16.10f, %16.10f, %16.10f, %16.10f\n", \
+           "Cast from " #STYPE, t1, t2, t3, t4, t5); \
+    fflush(stderr);\
+  } while (0);
 
 
 int main(void) {
@@ -146,10 +230,12 @@ int main(void) {
     const int nbrun = NBRUN;
     // "volatile" avoids optimizing out
     volatile unsigned char *_a, *_b, *_c;
+    
+    timing_cpuclock_init();
 
-    _a = xmalloc(memsize);
-    _b = xmalloc(memsize);
-    _c = xmalloc(memsize);
+    _a = malloc(memsize);
+    _b = malloc(memsize);
+    _c = malloc(memsize);
 
     /* initialization */
     srand(0);
@@ -157,73 +243,76 @@ int main(void) {
         _a[i] = rand() % (UCHAR_MAX - 1) + 1; // no zero div
         _b[i] = rand() % (UCHAR_MAX - 1) + 1; // no zero div
     }
-    INFO("%zu Kbytes, median of %d trials",
-         memsize / 1000, nbrun);
+    printf("%zu Kbytes, median of %d trials\n", memsize / 1000, nbrun);
     float t1, t2, t3, t4, t5;
 
-    INFO("Integer Arithmetics");
-    fprintf(stderr, "'%-20s', %16s, %16s, %16s, %16s\n", "Operation", "int8", "int16", "int32", "int64");
-    ITIME(c[i] = a[i] + b[i]);
-    times[ADD_FIX] = t3;
-    times[SUB_FIX] = t3;
-    ITIME(c[i] = a[i] & b[i]);
-    ITIME(c[i] = a[i] | b[i]);
-    ITIME(c[i] = a[i] ^ b[i]);
-    ITIME(c[i] = a[i] << 3);
-    times[CAST_FIX_FIX] = t3;
-    ITIME(c[i] = a[i] * b[i]);
-    times[MUL_FIX] = t4;
-    ITIME(c[i] = a[i] / b[i]);
-    times[DIV_FIX] = t4;
-    ITIME(c[i] = a[i] % b[i]);
-    times[REM_FIX] = t3;
+    printf("Integer Arithmetics\n");
+    printf("'%-20s', %16s, %16s, %16s, %16s\n", "Operation", "int8", "int16", "int32", "int64");
+    ITIME(OP_ADD, c[i] = a[i] + b[i]);
+    ITIME(OP_SUB, c[i] = a[i] - b[i]);
+    ITIME(OP_SHIFT, c[i] = a[i] << 3);
+    ITIME(OP_MUL, c[i] = a[i] * b[i]);
+    ITIME(OP_DIV, c[i] = a[i] / b[i]);
+    ITIME(OP_REM, c[i] = a[i] % b[i]);
 
-    INFO("Floating-point Arithmetics");
-    fprintf(stderr, "'%-20s', %16s, %16s, %16s, %16s\n", "Operation", "flt32", "flt64", "flt80", "flt128");
-    FTIME(c[i] = a[i] + b[i]);
-    times[ADD_FLOAT] = t1;
-    times[SUB_FLOAT] = t1;
-    times[ADD_DOUBLE] = t2;
-    times[SUB_DOUBLE] = t2;
-    FTIME(c[i] = a[i] * b[i]);
-    times[MUL_FLOAT] = t1;
-    times[MUL_DOUBLE] = t2;
-    FTIME(c[i] = a[i] / b[i]);
-    times[DIV_FLOAT] = t1;
-    times[DIV_DOUBLE] = t2;
-    FTIME(c[i] = fmod(a[i], b[i]));
-    times[REM_FLOAT] = t1;
-    times[REM_DOUBLE] = t2;
+    printf("Floating-point Arithmetics\n");
+    printf("'%-20s', %16s, %16s, %16s, %16s\n", "Operation", "flt32", "flt64", "flt80", "flt128");
+    FTIME(OP_ADD, c[i] = a[i] + b[i]);
+    FTIME(OP_SUB, c[i] = a[i] + b[i]);
+    FTIME(OP_MUL, c[i] = a[i] * b[i]);
+    FTIME(OP_DIV, c[i] = a[i] / b[i]);
+    F_FUN_TIME(OP_REM, c[i], fmodf, fmod, (a[i], b[i]));
 
-    fprintf(stderr, "'%-20s', %16s, %16s, %16s, %16s, %16s\n", " --- To --->", "flt32", "flt64", "int32", "flt80", "flt128");
-    CTIME(int32);
-    times[CAST_FIX_FLOAT] = t1+times[DIV_FLOAT];
-    times[CAST_FIX_DOUBLE] = t2+times[DIV_DOUBLE];
-    CTIME(flt32);
-    times[CAST_FLOAT_FIX] = t3+times[MUL_FLOAT];
-    times[CAST_FLOAT_DOUBLE] = t2;
-    CTIME(flt64);
-    times[CAST_DOUBLE_FLOAT] = t1;
-    times[CAST_DOUBLE_FIX] = t3+times[MUL_DOUBLE];
+    printf("'%-20s', %16s, %16s, %16s, %16s, %16s\n", " --- To --->", "flt32", "flt64", "int32", "flt80", "flt128");
+    CTIME(T_FIX, int32);
+    cast_times[T_FIX][T_FLOAT] += op_times[OP_DIV][T_FLOAT];
+    cast_times[T_FIX][T_DOUBLE] += op_times[OP_DIV][T_DOUBLE];
+    CTIME(T_FLOAT, flt32);
+    cast_times[T_FLOAT][T_FIX] += op_times[OP_MUL][T_FLOAT];
+    CTIME(T_DOUBLE, flt64);
+    cast_times[T_DOUBLE][T_FIX] += op_times[OP_MUL][T_DOUBLE];
 
-    xfree((void *) _a);
-    xfree((void *) _b);
-    xfree((void *) _c);
+    free((void *) _a);
+    free((void *) _b);
+    free((void *) _c);
 
     double min = 9E22;
-    for (int i = 0; i < COLLECTION_SIZE; i++) {
-        if (times[i] < min) {
-            min = times[i];
+    for (int i = 0; i < N_T; i++) {
+      for (int j = 0; j < N_OP; j++) {
+        if (op_times[j][i] < min && op_times[j][i] != 0) {
+          min = op_times[j][i];
         }
+      }
+      for (int j = 0; j < N_T; j++) {
+        if (cast_times[j][i] < min && cast_times[j][i] != 0) {
+          min = cast_times[j][i];
+        }
+      }
     }
 
-    for (int i = 0; i < COLLECTION_SIZE; i++) {
-        times[i] /= min;
+    for (int i = 0; i < N_T; i++) {
+      for (int j = 0; j < N_OP; j++) {
+        op_times[j][i] /= min;
+      }
+      for (int j = 0; j < N_T; j++) {
+        cast_times[j][i] /= min;
+      }
     }
 
-    for (int i = 0; i < COLLECTION_SIZE; i++) {
-        printf("%s,\t%lf\n", coll[i], times[i]);
+    for (int i = 0; i < N_OP; i++) {
+      for (int j = 0; j < N_T; j++) {
+        if (op_times[i][j] > 0) {
+          printf("%s_%s,\t%lf\n", ops_names[i], types_names[j], op_times[i][j]);
+        }
+      }
+    }
+    for (int i=0; i<N_T; i++) {
+      for (int j=0; j<N_T; j++) {
+        if (cast_times[i][j] > 0) {
+          printf("CAST_%s_%s,\t%lf\n", types_names[i], types_names[j], cast_times[i][j]);
+        }
+      }
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
--- tool/taffo-costmodel/timing.c
+++ tool/taffo-costmodel/timing.c
@@ -16,11 +16,6 @@
  * @author Nicolas Limare <nicolas@limare.net>
  */
 
-#define _POSIX_C_SOURCE 200112L /* ask for POSIX 2001 */
-
-#ifndef USE_TIMING
-#define USE_TIMING
-#endif
 #include "timing.h"
 
 /*
@@ -59,58 +54,6 @@
 
 
 /****************************************************************
- * WALL CLOCK TIMER
- ****************************************************************/
-
-/** number of wall clock counters */
-#ifndef TIMING_WALLCLOCK_NB
-#define TIMING_WALLCLOCK_NB 16
-#endif
-
-/** wall clock counter array, initialized to 0 */
-unsigned long _timing_wallclock_counter[TIMING_WALLCLOCK_NB];
-
-/**
- * @brief portable wallclock call
- */
-#if defined(TIMING_POSIX)
-/* use POSIX gettomeofday() with microsecond precision */
-unsigned long _timing_wallclock() {
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return (unsigned long)(t.tv_usec + t.tv_sec * 1000000);
-}
-#elif defined(TIMING_WINDOWS)
-/* use Windows GetSystemTime() with millisecond precision */
-unsigned long _timing_wallclock() {
-    static SYSTEMTIME t;
-    GetSystemTime(&t);
-#define _UL unsigned long       /* temporary, for shorter lines */
-    return (_UL)(1000 * ((_UL) t.wMilliseconds
-                         + 1000 * ((_UL) t.wSecond
-                                   + 60 * ((_UL) t.wMinute
-                                           + 60 * ((_UL) t.wHour
-                                                   + 24 * (_UL) t.wDay)))));
-#undef _UL
-}
-#else
-/* fall back to libc time() with second precision */
-#warning wallclock uses a low-precision libc implementation
-unsigned long _timing_wallclock() {
-    time_t rawtime;
-    struct tm *t;
-    (void) time(&rawtime);
-    t = localtime(&rawtime);
-#define _UL unsigned long       /* temporary, for shorter lines */
-    return (_UL)(1000000 * ((_UL) t->tm_sec
-                            + 60 * ((_UL) t->tm_min +
-                                    +60 * ((_UL) t->tm_hour +
-                                           +24 * (_UL) t->tm_mday))));
-#undef _UL
-}
-#endif
-
-/****************************************************************
  * CPU CLOCK TIMER
  ****************************************************************/
 
@@ -122,12 +65,18 @@ unsigned long _timing_wallclock() {
 /** CPU clock counter array, initialized to 0 (K&R2, p.86) */
 unsigned long _timing_cpuclock_counter[TIMING_CPUCLOCK_NB];
 
+void timing_cpuclock_init(void)
+{
+  return;
+}
+
 /**
  * @brief portable cpuclock call
  */
 #if defined(TIMING_POSIX)
 /* use POSIX clock_gettime (reduced to microsecond precision) */
-unsigned long _timing_cpuclock() {
+unsigned long _timing_cpuclock()
+{
     struct timespec tmp;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tmp);
     return (unsigned long)(tmp.tv_sec * 1000000 + tmp.tv_nsec / 1000);
@@ -139,57 +88,3 @@ unsigned long _timing_cpuclock()
     return (unsigned long)(clock() * 1000000 / CLOCKS_PER_SEC);
 }
 #endif
-
-/****************************************************************
- * CPU CYCLES COUNTER
- ****************************************************************/
-
-/**
- * Counter functions use the work of Daniel J. Bernstein:
- *   http://ebats.cr.yp.to/cpucycles.html
- *   http://ebats.cr.yp.to/cpucycles-20060326.tar.gz
- */
-
-#if (defined(__STDC__) && defined(__STDC_VERSION__) \
-     && (__STDC_VERSION__ >= 199409L))
-#define _LL long long
-#else
-#define _LL long
-#endif
-
-/** number of cycle counters */
-#ifndef TIMING_CYCLE_NB
-#define TIMING_CYCLE_NB 16
-#endif
-
-/** cycle counter array, initialized to 0 */
-_LL _timing_cycle_counter[TIMING_CYCLE_NB];
-
-/**
- * @brief portable cycles counter
- */
-_LL _timing_cpucycles(void);
-#if defined(TIMING_AMD64)
-/** CPU cycles counter for amd64 */
-_LL _timing_cpucycles() {
-    unsigned _LL result;
-    __asm__ volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax":"=a"
-                     (result)::"%rdx");
-    return result;
-}
-
-#elif defined(TIMING_I386)
-/** CPU cycles counter for x86 */
-_LL _timing_cpucycles() {
-    _LL result;
-    __asm__ volatile(".byte 15;.byte 49":"=A"(result));
-    return result;
-}
-
-#else
-/** dummy CPU cycles counter */
-_LL _timing_cpucycles() {
-    return 0;
-}
-
-#endif                          /* architecture detection */
