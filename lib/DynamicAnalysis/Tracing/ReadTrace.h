dd22f87be7c88f8ff518a667833f52707ac35521
denisovlev <lev.denisov@polimi.it>
2022-04-27 12:16:55 +0200

    connected components on memory operations

--- lib/DynamicAnalysis/Tracing/ReadTrace.h
+++ lib/DynamicAnalysis/Tracing/ReadTrace.h
@@ -16,6 +16,13 @@ struct ReadTrace : public llvm::PassInfoMixin<ReadTrace> {
   bool runOnModule(llvm::Module &M);
 
 private:
+
+  int buildMemEdgesList(llvm::Module &M, std::unordered_map<llvm::Value*, int>& instToIndex,
+                         std::unordered_map<int, llvm::Value*>& indexToInst,
+                         std::list<std::pair<int, int>>& edges);
+
+  void connectedComponents(int count, std::list<std::pair<int, int>>& edges, std::map<int, std::list<int>>& cc);
+
   void
   parseTraceFiles(std::unordered_map<std::string, double>& minVals, std::unordered_map<std::string, double>& maxVals,
                   std::unordered_map<std::string, mdutils::FloatType::FloatStandard>& valTypes) const;
