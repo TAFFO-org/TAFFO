54e7f5ea63578b9c437f8f8b9c4d7aa91db8e971
denisovlev <lev.denisov@polimi.it>
2022-05-11 16:02:21 +0200

    separate ConnectedComponents

---
+++ lib/DynamicAnalysis/Tracing/ConnectedComponents.h
@@ -0,0 +1,31 @@
+#ifndef TAFFO_CONNECTEDCOMPONENTS_H
+#define TAFFO_CONNECTEDCOMPONENTS_H
+
+#include <list>
+#include <unordered_map>
+#include <memory>
+
+namespace taffo
+{
+
+class ConnectedComponents
+{
+public:
+  ConnectedComponents(const int NodeCount, const std::list<std::pair<int, int>> &Edges);
+
+  const std::unordered_map<int, std::list<int>>& getResult() {
+      return cc;
+  };
+
+private:
+  const int nodeCount;
+  const std::list<std::pair<int, int>> &edges;
+  std::unordered_map<int, std::list<int>> cc;
+
+  void calculateConnectedComponents();
+  int merge(int* parent, int x);
+};
+
+} // namespace taffo
+
+#endif // TAFFO_CONNECTEDCOMPONENTS_H
