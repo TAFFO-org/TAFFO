77686372f4fd3b5768c274dc224961cfbce9722c
denisovlev <lev.denisov@polimi.it>
2022-06-28 17:39:17 +0200

    use taffo dynamic-instrument and dynamic-trace options in axbench

--- test/axbench/compile+collect.sh
+++ test/axbench/compile+collect.sh
@@ -69,17 +69,33 @@ if [[ -z $DONT_REBUILD ]]; then
 
   dynamic_analysis() {
     taffo -temp-dir obj \
-      -o bin/${bench}.out.dynamic_final \
-      -O3 \
-      -dynamic-training-data \
-      /home/denisovlev/Projects/TAFFO/test/axbench/blackscholes/data/input/blackscholesTrain_100K.data \
+      -o bin/${bench}.out.dynamic_instrumented \
+      -O0 -disable-O0-optnone \
+      -dynamic-instrument \
       -I../common/src \
       ${files} \
       ${debug} \
       ${errorprop} \
       ${no_mem2reg} \
       -lm \
-      2> stats/taffo.log
+      2> stats/taffo1.log
+
+    "bin/${bench}.out.dynamic_instrumented" \
+    /home/denisovlev/Projects/TAFFO/test/axbench/blackscholes/data/input/blackscholesTrain_100K.data \
+    /dev/null \
+     > "obj/${bench}.out.instrumented.trace"
+
+    taffo -temp-dir obj \
+      -o bin/${bench}.out.dynamic_final \
+      -O3 \
+      -dynamic-trace "obj/${bench}.out.instrumented.trace" \
+      -I../common/src \
+      ${files} \
+      ${debug} \
+      ${errorprop} \
+      ${no_mem2reg} \
+      -lm \
+      2> stats/taffo2.log
   }
 
   dynamic_analysis
