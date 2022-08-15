308e52971bc8ef83a8c76f8b7f5a5728dc305c96
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2022-08-15 17:38:31 +0100

    Test/Polybench-ACC-NVCL: Use the same version of LLVM for all compilation commands.

--- test/polybench-acc-nvcl/utilities/common.mk
+++ test/polybench-acc-nvcl/utilities/common.mk
@@ -2,14 +2,15 @@ INCLUDE=-I${PATH_TO_UTILS}
 EXECUTABLE_TAFFO=$(patsubst %.exe,%.taffo.exe,${EXECUTABLE})
 PTXFILE=$(patsubst %.cl,%.ptx,${CLFILE})
 PTXFILE_TAFFO=$(patsubst %.cl,%.taffo.ptx,${CLFILE})
-CLANG=$(shell taffo -print-clang)
+CLANG:=$(shell taffo -print-llvm-bin-dir)/clang
+LLVM_LINK:=$(shell taffo -print-llvm-bin-dir)/llvm-link
 LIB=-lOpenCL -lm
 
 .PHONY: all
 all: ${EXECUTABLE} ${PTXFILE} ${EXECUTABLE_TAFFO} ${PTXFILE_TAFFO}
 
 ${EXECUTABLE}:
-	clang-14 -O3 -DPOLYBENCH_STACK_ARRAYS -I${PATH_TO_UTILS} ${LIB} ${CFLAGS} ${CFILES} -o ${EXECUTABLE}
+	$(CLANG) -O3 -DPOLYBENCH_STACK_ARRAYS -I${PATH_TO_UTILS} ${LIB} ${CFLAGS} ${CFILES} -o ${EXECUTABLE}
 
 ${EXECUTABLE_TAFFO}:
 	mkdir -p taffo_drvr_logs
@@ -18,13 +19,13 @@ ${EXECUTABLE_TAFFO}:
 
 ${PTXFILE}:
 	$(CLANG) \
-		-Xclang -finclude-default-header -Xclang -fdeclare-opencl-builtins -Xclang -disable-O0-optnone\
+		-Xclang -finclude-default-header -Xclang -fdeclare-opencl-builtins -Xclang -disable-O0-optnone -Xclang -no-opaque-pointers\
 		-D__cl_clang_storage_class_specifiers \
 		-target nvptx64-unknown-nvcl \
 		-O0 -march=sm_86 -S -emit-llvm \
 		-o ${CLFILE}.ll \
 		${CLFILE}
-	llvm-link-14 \
+	$(LLVM_LINK) \
 		-S -o ${CLFILE}.linked.ll \
 		${CLFILE}.ll \
 		/usr/lib/clc/nvptx64--nvidiacl.bc
@@ -39,7 +40,7 @@ ${PTXFILE_TAFFO}: ${PTXFILE}
 		-S -emit-llvm -oclkern -Xdta -notypemerge -target nvptx64-unknown-nvcl -temp-dir taffo_kern_logs -debug \
 		-o ${CLFILE}.taffo.ll \
 			2> taffo_kern_logs/taffo.log
-	llvm-link-14 \
+	$(LLVM_LINK) \
 		-S -o ${CLFILE}.taffo.linked.ll \
 		${CLFILE}.taffo.ll \
 		/usr/lib/clc/nvptx64--nvidiacl.bc
