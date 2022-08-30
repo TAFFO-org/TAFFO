715bf34b2481ee280256fe8b9e9741662aa47f06
Domenico Cacace <domenico.cacace@mail.polimi.it>
2022-08-30 17:12:00 +0200

    VRA: add tests for Range and RangeNode fetching

--- unittests/RangeAnalysis/TaffoVRA/VRAStoreTest.cpp
+++ unittests/RangeAnalysis/TaffoVRA/VRAStoreTest.cpp
@@ -136,4 +136,117 @@ TEST_F(VRAStoreTest, convexMerge_distinctStruct)
   EXPECT_FALSE(scalar->isFinal());
 }
 
+TEST_F(VRAStoreTest, fetchRange_scalar)
+{
+  auto V1 = ConstantInt::get(Type::getInt32Ty(Context), 1);
+  auto N1 = new VRAScalarNode(std::make_shared<range_t>(range_t{1, 2, false}));
+  VRAs.setNode(V1, std::make_shared<VRAScalarNode>(*N1));
+
+  auto range = VRAs.fetchRange(V1);
+  ASSERT_NE(range, nullptr);
+  EXPECT_EQ(range->min(), 1);
+  EXPECT_EQ(range->max(), 2);
+  EXPECT_FALSE(range->isFinal());
+}
+
+TEST_F(VRAStoreTest, fetchRange_struct)
+{
+  auto V1 = ConstantInt::get(Type::getInt32Ty(Context), 1);
+  auto N1 = new VRAStructNode();
+  N1->setNodeAt(0, std::make_shared<VRAScalarNode>(std::make_shared<range_t>(range_t{1, 2, false})));
+  VRAs.setNode(V1, std::make_shared<VRAStructNode>(*N1));
+
+  auto range = VRAs.fetchRange(V1);
+  ASSERT_EQ(range, nullptr);
+}
+
+TEST_F(VRAStoreTest, fetchRangeNode_scalar)
+{
+  auto V1 = ConstantInt::get(Type::getInt32Ty(Context), 1);
+  auto N1 = new VRAScalarNode(std::make_shared<range_t>(range_t{1, 2, false}));
+  VRAs.setNode(V1, std::make_shared<VRAScalarNode>(*N1));
+
+  auto node = VRAs.fetchRangeNode(V1);
+  ASSERT_NE(node, nullptr);
+  auto scalar = std::dynamic_ptr_cast_or_null<VRAScalarNode>(node);
+  ASSERT_NE(scalar, nullptr);
+  EXPECT_EQ(scalar->getRange()->min(), 1);
+  EXPECT_EQ(scalar->getRange()->max(), 2);
+  EXPECT_FALSE(scalar->isFinal());
+}
+
+TEST_F(VRAStoreTest, fetchRangeNode_scalarPtr)
+{
+  // TODO: how do I get a pointer value?
+  /*
+    auto V1 = ConstantInt::get(Type::getInt32PtrTy(Context), 1);
+    auto parent = new VRAScalarNode(std::make_shared<range_t>(range_t{1, 2, false}));
+    auto N1 = new VRAPtrNode(std::make_shared<VRAScalarNode>(*parent));
+    VRAs.setNode(V1, std::make_shared<VRAPtrNode>(*N1));
+
+    auto node = VRAs.fetchRangeNode(V1);
+    ASSERT_NE(node, nullptr);
+    auto scalar = std::dynamic_ptr_cast_or_null<VRAScalarNode>(node);
+    ASSERT_NE(scalar, nullptr);
+    EXPECT_EQ(scalar->getRange()->min(), 1);
+    EXPECT_EQ(scalar->getRange()->max(), 2);
+    EXPECT_FALSE(scalar->isFinal());
+    */
+}
+
+TEST_F(VRAStoreTest, fetchRangeNode_struct)
+{
+  auto V1 = ConstantInt::get(Type::getInt32Ty(Context), 1);
+  auto N1 = new VRAStructNode();
+  N1->setNodeAt(0, std::make_shared<VRAScalarNode>(std::make_shared<range_t>(range_t{1, 2, false})));
+  VRAs.setNode(V1, std::make_shared<VRAStructNode>(*N1));
+
+  auto node = VRAs.fetchRangeNode(V1);
+  ASSERT_EQ(node, nullptr);
+}
+
+TEST_F(VRAStoreTest, fetchRange_NodePtr_scalar)
+{
+  auto node = new VRAScalarNode(std::make_shared<range_t>(range_t{1, 2, true}));
+
+  auto ret = VRAs.fetchRange(std::make_shared<VRAScalarNode>(*node));
+  auto range = std::dynamic_ptr_cast_or_null<VRAScalarNode>(ret);
+  ASSERT_NE(range, nullptr);
+  EXPECT_EQ(range->getRange()->min(), 1);
+  EXPECT_EQ(range->getRange()->max(), 2);
+  EXPECT_TRUE(range->isFinal());
+}
+
+TEST_F(VRAStoreTest, fetchRange_NodePtr_struct)
+{
+  auto node = new VRAStructNode();
+  node->setNodeAt(0, std::make_shared<VRAScalarNode>(*new VRAScalarNode(std::make_shared<range_t>(range_t{1, 2, true}))));
+
+  auto ret = VRAs.fetchRange(std::make_shared<VRAStructNode>(*node));
+  auto range = std::dynamic_ptr_cast_or_null<VRAStructNode>(ret);
+  ASSERT_NE(range, nullptr);
+  EXPECT_EQ(range->fields().size(), 1);
+  auto field = std::dynamic_ptr_cast_or_null<VRAScalarNode>(range->getNodeAt(0));
+  ASSERT_NE(field, nullptr);
+  EXPECT_EQ(field->getRange()->min(), 1);
+  EXPECT_EQ(field->getRange()->max(), 2);
+  EXPECT_TRUE(field->getRange()->isFinal());
+}
+
+TEST_F(VRAStoreTest, fetchRange_NodePtr_GEP)
+{
+  auto node = new VRAStructNode();
+  node->setNodeAt(0, std::make_shared<VRAScalarNode>(*new VRAScalarNode(std::make_shared<range_t>(range_t{1, 2, true}))));
+  node->setNodeAt(1, std::make_shared<VRAScalarNode>(*new VRAScalarNode(std::make_shared<range_t>(range_t{3, 4, true}))));
+  unsigned offset[2] = {1, 1};
+  auto GEP = new VRAGEPNode(std::make_shared<VRAStructNode>(*node), offset);
+
+  auto ret = VRAs.fetchRange(std::make_shared<VRAGEPNode>(*GEP));
+  auto range = std::dynamic_ptr_cast_or_null<VRAScalarNode>(ret);
+  ASSERT_NE(range, nullptr);
+  EXPECT_EQ(range->getRange()->min(), 3);
+  EXPECT_EQ(range->getRange()->max(), 4);
+  EXPECT_TRUE(range->getRange()->isFinal());
+}
+
 } // namespace
