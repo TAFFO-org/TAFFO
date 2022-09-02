82f8b9323643cedaf98729126f584e65aadc6c4c
ilgeco <gabriele.magnani@polimi.it>
2022-09-02 17:48:26 +0200

    fix lead lag

--- test/fpbench/leadLag/leadLag.c
+++ test/fpbench/leadLag/leadLag.c
@@ -9,46 +9,47 @@
 #define M 100
 #endif
 
-float ex0(float y, float yd) {
-  float eps = 0.01f;
-  float Dc = -1280.0f;
-  float Ac00 = 0.499f;
-  float Ac01 = -0.05f;
-  float Ac10 = 0.01f;
-  float Ac11 = 1.0f;
-  float Bc0 = 1.0f;
-  float Bc1 = 0.0f;
-  float Cc0 = 564.48f;
-  float Cc1 = 0.0f;
-  float yc = 0.0f;
-  float u = 0.0f;
-  float xc0 = 0.0f;
-  float xc1 = 0.0f;
+float ex0(float y, float yd)
+{
+
+  float __attribute__((annotate("scalar(range(-50, 50))"))) yc = 0.0f;
+  float __attribute__((annotate("scalar(range(-2000, 2000))"))) u = 0.0f;
+  float __attribute__((annotate("scalar(range(-2, 2))"))) xc0 = 0.0f;
+  float __attribute__((annotate("scalar(range(-4, 4))"))) xc1 = 0.0f;
   float i = 0.0f;
-  float e = 1.0f;
-  int tmp = e > eps;
+  float __attribute__((annotate("scalar(range(-4, 4))"))) e = 1.0f;
+  int tmp = e > 0.01f;
+  float v = y - yd;
+  float __attribute__((annotate("scalar(range(-4, 4))"))) pre_abs = 0.0f;
+  float __attribute__((annotate("target('tmp') scalar(range(-50, 50))"))) tmp_1;
+  if (v < -1.0f) {
+    tmp_1 = -1.0f;
+  } else if (1.0f < v) {
+    tmp_1 = 1.0f;
+  } else {
+    tmp_1 = v;
+  }
   while (tmp) {
-    float v = y - yd;
-    float tmp_1;
-    if (v < -1.0f) {
-      tmp_1 = -1.0f;
-    } else if (1.0f < v) {
-      tmp_1 = 1.0f;
-    } else {
-      tmp_1 = v;
-    }
     yc = tmp_1;
-    u = (Cc0 * xc0) + ((Cc1 * xc1) + (Dc * yc));
-    xc0 = (Ac00 * xc0) + ((Ac01 * xc1) + (Bc0 * yc));
-    xc1 = (Ac10 * xc0) + ((Ac11 * xc1) + (Bc1 * yc));
+
+    u = (564.48f * xc0) + (-1280.0f * yc);
+
+    xc0 = (0.499f * xc0) + (-0.05f * xc1 + yc);
+    xc1 = (0.01f * xc0) + (xc1);
     i = i + 1.0f;
-    e = fabsf((yc - xc1));
-    tmp = e > eps;
+    pre_abs = yc - xc1;
+    if (pre_abs < 0) {
+      e = -pre_abs;
+    } else {
+      e = pre_abs;
+    }
+    tmp = e > 0.01f;
   }
   return xc1;
 }
 
-int main() {
+int main()
+{
   static const int len = sizeof(arr) / sizeof(arr[0]) / 2;
   float __attribute__((annotate("target('main') scalar(range(0, 50))"))) y[len];
   float __attribute__((annotate("scalar(range(0, 50))"))) yd[len];
@@ -88,6 +89,7 @@ int main() {
       printf("Cycles: %li\n", end - start);
     }
   }
+
   printf("Values Begin\n");
   for (int j = 0; j < len; ++j) {
     printf("%f\n", res[j]);
