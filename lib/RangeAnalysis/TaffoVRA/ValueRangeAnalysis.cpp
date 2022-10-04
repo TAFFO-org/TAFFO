511acbf7f530223a98d9c8df2a1021db34aa4a47
Domenico Cacace <domenico.cacace@mail.polimi.it>
2022-10-04 18:23:57 +0200

    VRA: move opt command line options in ValueRangeAnalysis.cpp

--- lib/RangeAnalysis/TaffoVRA/ValueRangeAnalysis.cpp
+++ lib/RangeAnalysis/TaffoVRA/ValueRangeAnalysis.cpp
@@ -11,23 +11,26 @@ using namespace llvm;
 using namespace taffo;
 using namespace mdutils;
 
-cl::opt<bool> taffo::PropagateAll("propagate-all",
-                                  cl::desc("Propagate ranges for all functions, "
-                                           "not only those marked as starting point."),
-                                  cl::init(false));
-cl::opt<unsigned> taffo::Unroll("unroll",
-                                cl::desc("Default loop unroll count. "
-                                         "Setting this to 0 disables loop unrolling. "
-                                         "(Default: 1)"),
-                                cl::value_desc("count"),
-                                cl::init(1U));
+namespace taffo
+{
+llvm::cl::opt<bool> PropagateAll("propagate-all",
+                                 llvm::cl::desc("Propagate ranges for all functions, "
+                                                "not only those marked as starting point."),
+                                 llvm::cl::init(false));
+llvm::cl::opt<unsigned> Unroll("unroll",
+                               llvm::cl::desc("Default loop unroll count. "
+                                              "Setting this to 0 disables loop unrolling. "
+                                              "(Default: 1)"),
+                               llvm::cl::value_desc("count"),
+                               llvm::cl::init(1U));
 
-cl::opt<unsigned> taffo::MaxUnroll("max-unroll",
-                                   cl::desc("Max loop unroll count. "
-                                            "Setting this to 0 disables loop unrolling. "
-                                            "(Default: 256)"),
-                                   cl::value_desc("count"),
-                                   cl::init(256U));
+llvm::cl::opt<unsigned> MaxUnroll("max-unroll",
+                                  llvm::cl::desc("Max loop unroll count. "
+                                                 "Setting this to 0 disables loop unrolling. "
+                                                 "(Default: 256)"),
+                                  llvm::cl::value_desc("count"),
+                                  llvm::cl::init(256U));
+}
 
 PreservedAnalyses ValueRangeAnalysis::run(Module &M, ModuleAnalysisManager &AM)
 {
