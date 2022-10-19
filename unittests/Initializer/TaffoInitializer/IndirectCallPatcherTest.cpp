a9d05ebd5048a7075dd9f47eef48de46788cff5d
Domenico Cacace <domenico.cacace@mail.polimi.it>
2022-10-19 17:56:22 +0200

    Initializer: generalize test for unsupported calls

--- unittests/Initializer/TaffoInitializer/IndirectCallPatcherTest.cpp
+++ unittests/Initializer/TaffoInitializer/IndirectCallPatcherTest.cpp
@@ -14,6 +14,9 @@ class IndirectCallPatcherTest : public taffo_test::Test
   protected:
     Function *F0;
     BasicBlock *BB0;
+    const std::vector<std::string> prefixBlocklist{"__kmpc_omp_task",
+                                                    "__kmpc_reduce"};
+
 
   IndirectCallPatcherTest() {
     F0 = genFunction(*M, "caller", Type::getVoidTy(Context), {});
@@ -23,24 +26,31 @@ class IndirectCallPatcherTest : public taffo_test::Test
 
 TEST_F(IndirectCallPatcherTest, containsUnsupportedFunction_lastFunction)
 {
-  auto F = genFunction(*M, "__kmpc_omp_task_fun", Type::getVoidTy(Context), {});
-  auto CI = CallInst::Create(F, {}, "", BB0);
-  ASSERT_TRUE(containsUnsupportedFunctions(F0, {}));
-
-  F = genFunction(*M, "__kmpc_reduce_fun", Type::getVoidTy(Context), {});
-  CI = CallInst::Create(F, {}, "", BB0);
-  ASSERT_TRUE(containsUnsupportedFunctions(F0, {}));
+  for (auto prefix : prefixBlocklist) {
+    auto F1 = genFunction(*M, prefix, Type::getVoidTy(Context), {});
+    auto CI1 = CallInst::Create(F1, {}, "", BB0);
+    ASSERT_TRUE(containsUnsupportedFunctions(F0, {}));
+  }
 }
 
 TEST_F(IndirectCallPatcherTest, containsUnsupportedFunction_traversed)
 {
-  auto F = genFunction(*M, "supportedFunction", Type::getVoidTy(Context), {});
-  auto F1 = genFunction(*M, "__kmpc_omp_task_fun", Type::getVoidTy(Context), {});
-  auto BB = BasicBlock::Create(Context, "entry", F);
-  auto CI = CallInst::Create(F, {}, "", BB0);
-  auto CI1 = CallInst::Create(F1, {}, "", BB);
-  ASSERT_TRUE(containsUnsupportedFunctions(F0, {}));
-  ASSERT_TRUE(containsUnsupportedFunctions(F, {}));
+  /*
+   * F0: entry
+   * F1: supportedFunction
+   * F2: unsupported function
+   * Calls: F0 -> F1 -> F2
+   */
+
+  for (auto prefix : prefixBlocklist) {
+    auto F1 = genFunction(*M, "supportedFunction", Type::getVoidTy(Context), {});
+    auto F2 = genFunction(*M, prefix, Type::getVoidTy(Context), {});
+    auto BB1 = BasicBlock::Create(Context, "entry", F1);
+    auto CI1 = CallInst::Create(F1, {}, "", BB0);
+    auto CI2 = CallInst::Create(F2, {}, "", BB1);
+    ASSERT_TRUE(containsUnsupportedFunctions(F0, {}));
+    ASSERT_TRUE(containsUnsupportedFunctions(F1, {}));
+  }
 }
 
 TEST_F(IndirectCallPatcherTest, containsUnsupportedFunction_no)
