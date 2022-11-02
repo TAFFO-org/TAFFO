a8c4a0d4d289551f824d40ff75b344e4097e2dbc
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2022-11-02 12:43:18 +0000

    Polybench/ACC: More fixes to the experiment script. Save raw data for the benchmarks.

--- test/polybench-acc-nvcl/experiment.sh
+++ test/polybench-acc-nvcl/experiment.sh
@@ -2,6 +2,7 @@
 
 exp_dir=_experiment_$(date +%Y-%m-%d_%H-%M-%S)
 mkdir ${exp_dir}
+raw_data_dir=${exp_dir}/raw_data
 
 host_dta_set='fixp f32 mixed'
 kern_args_set='fixp f32 f16 mixed'
@@ -13,7 +14,8 @@ kern_dta_set='fixp f32 f16 mixed'
 for host_dta in $host_dta_set; do
   for kern_args in $kern_args_set; do
     for kern_dta in $kern_dta_set; do
-      exp_file_prefix="${exp_dir}/${host_dta}_${kern_args}_${kern_dta}"
+      conf="${host_dta}_${kern_args}_${kern_dta}"
+      exp_file_prefix="${exp_dir}/${conf}"
       echo host_dta=${host_dta} kern_args=${kern_args} kern_dta=${kern_dta}
       make clean &> /dev/null || continue
 
@@ -27,6 +29,10 @@ for host_dta in $host_dta_set; do
       
       echo run
       make -s run &> ${exp_file_prefix}_run.txt || continue
+
+      raw=$(pwd)/${raw_data_dir}/${conf}
+      mkdir -p ${raw}
+      find . -regex '\./[^_].*\.exe\.txt' -execdir cp '{}' ${raw} ';'
 
       echo validate
       make -s validate &> ${exp_file_prefix}_validate.txt || continue
