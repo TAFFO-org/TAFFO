a799f34eff8f2c3f0eb29a2dc86f2385814a299f
denisovlev <lev.denisov@polimi.it>
2022-11-03 11:50:05 +0100

    test for error estimation: analysis script

--- test/error_estimation/src/generate_data.py
+++ test/error_estimation/src/generate_data.py
@@ -4,7 +4,7 @@ import sys
 count = int(sys.argv[1])
 random.seed(42)
 # numbers = [random.uniform(-1, 10) for i in range(count)]
-r = random.random()
+r = random.random() + 1
 numbers = [r for i in range(count)]
 for n in numbers:
     print(f"{str(n)}")
