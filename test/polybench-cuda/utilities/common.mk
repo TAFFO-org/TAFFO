9b4cfeb99af0af85eb03aca82a5abecae2c1d1fb
maggio98 <10572955@polimi.it>
2023-01-11 20:01:26 +0100

    Test/Polybench-cuda: add experiment setup

--- test/polybench-cuda/utilities/common.mk
+++ test/polybench-cuda/utilities/common.mk
@@ -6,6 +6,57 @@ CLANG:=$(shell taffo -print-llvm-bin-dir)/clang
 CLANGX:=$(shell taffo -print-llvm-bin-dir)/clang++
 LLVM_NVPTX:=$(shell taffo -print-llvm-bin-dir)/llc
 
+TAFFO_HOST_DTA    ?=fixp
+TAFFO_KERN_ARGS   ?=fixp
+TAFFO_KERN_DTA    ?=fixp
+
+ifeq ($(TAFFO_HOST_DTA),fixp)
+endif
+ifeq ($(TAFFO_HOST_DTA),f16)
+TAFFO_EXEC_OPTS   += -Xdta -usefloat -Xdta f16
+endif
+ifeq ($(TAFFO_HOST_DTA),f32)
+TAFFO_EXEC_OPTS   += -Xdta -usefloat -Xdta f32
+endif
+ifeq ($(TAFFO_HOST_DTA),mixed) #ilp
+TAFFO_EXEC_OPTS   += -mixedmode -costmodel i7-4 -instructionset fix
+endif
+
+ifeq ($(TAFFO_KERN_ARGS),fixp)
+TAFFO_EXEC_OPTS   += -Xdta -bufferid-import -Xdta bufferid_conf_fixp32.yaml
+TAFFO_KERN_OPTS   += -Xdta -bufferid-import -Xdta bufferid_conf_fixp32.yaml
+endif
+ifeq ($(TAFFO_KERN_ARGS),f16)
+TAFFO_EXEC_OPTS   += -Xdta -bufferid-import -Xdta bufferid_conf_fp16.yaml
+TAFFO_KERN_OPTS   += -Xdta -bufferid-import -Xdta bufferid_conf_fp16.yaml
+endif
+ifeq ($(TAFFO_KERN_ARGS),f32)
+TAFFO_EXEC_OPTS   += -Xdta -bufferid-import -Xdta bufferid_conf_fp32.yaml
+TAFFO_KERN_OPTS   += -Xdta -bufferid-import -Xdta bufferid_conf_fp32.yaml
+endif
+ifeq ($(TAFFO_KERN_ARGS),mixed)
+TAFFO_EXEC_OPTS   += -Xdta -bufferid-import -Xdta taffo_kern_logs/bufferid.yaml
+TAFFO_KERN_OPTS   += -Xdta -bufferid-export -Xdta taffo_kern_logs/bufferid.yaml
+endif
+
+ifeq ($(TAFFO_KERN_DTA),fixp)
+endif
+ifeq ($(TAFFO_KERN_DTA),f16)
+TAFFO_KERN_OPTS   += -Xdta -usefloat -Xdta f16
+endif
+ifeq ($(TAFFO_KERN_DTA),f32)
+TAFFO_KERN_OPTS   += -Xdta -usefloat -Xdta f32
+endif
+ifeq ($(TAFFO_KERN_DTA),mixed) #ilp
+TAFFO_KERN_OPTS   += -mixedmode -costmodel nv_sm86 -instructionset gpu -Xdta -mixedtuningenob -Xdta 1 -Xdta -mixedtuningtime -Xdta 10000 -Xdta -mixedtuningcastingtime -Xdta 10000
+endif
+
+ifeq ($(MAKECMDGOALS),)
+$(info Host DTA config type    = $(TAFFO_HOST_DTA))
+$(info Kernel args config type = $(TAFFO_KERN_ARGS))
+$(info Kernel DTA config type  = $(TAFFO_KERN_DTA))
+endif
+
 .PHONY: all
 all: ${EXECUTABLE} ${PTXFILE} ${EXECUTABLE_TAFFO} ${PTXFILE_TAFFO}
 
@@ -15,11 +66,9 @@ ${EXECUTABLE}:  ${PTXFILE}
 
 ${EXECUTABLE_TAFFO}: ${PTXFILE_TAFFO}
 	mkdir -p taffo_drvr_logs
-	taffo -O3 -DPOLYBENCH_STACK_ARRAYS --cuda-host-only -S\
-    -Xvra -max-unroll=0 \
-    -Xdta -notypemerge -Xclang -no-opaque-pointers\
-    -Xdta -bufferid-import -Xdta taffo_kern_logs/bufferid.yaml -I/usr/local/cuda/include \
-    -I${PATH_TO_UTILS} ${CFLAGS} ${CFILES} -o ${EXECUTABLE_TAFFO}.s -temp-dir taffo_drvr_logs -debug \
+	taffo -O3 -DPOLYBENCH_STACK_ARRAYS --cuda-host-only -S \
+    -Xvra -max-unroll=0 $(TAFFO_EXEC_OPTS)\
+    -I/usr/local/cuda/include -I${PATH_TO_UTILS} ${CFLAGS} ${CFILES} -o ${EXECUTABLE_TAFFO}.s -temp-dir taffo_drvr_logs -debug \
         2> taffo_drvr_logs/taffo.log
 	${CLANG} -O3 --cuda-host-only -L/usr/local/cuda/lib64 -lstdc++ -lm \
     -lcudart_static -ldl -lrt -pthread ${CFLAGS} ${EXECUTABLE_TAFFO}.s -o ${EXECUTABLE_TAFFO} -lcuda
@@ -29,10 +78,9 @@ ${PTXFILE}:
 
 ${PTXFILE_TAFFO}:
 	mkdir -p taffo_kern_logs
-	taffo ${CUFILE}  -I${PATH_TO_UTILS}\
-		-S -emit-llvm -cudakern -Xdta -notypemerge -temp-dir taffo_kern_logs -debug -Xclang -no-opaque-pointers --cuda-device-only --cuda-gpu-arch=sm_86\
-		-Xdta -bufferid-export -Xdta taffo_kern_logs/bufferid.yaml \
-		-o ${CUFILE}.taffo.ll \
+	taffo ${CUFILE} -I${PATH_TO_UTILS} \
+	-S -emit-llvm -cudakern -temp-dir taffo_kern_logs -debug --cuda-device-only --cuda-gpu-arch=sm_86 \
+	$(TAFFO_KERN_OPTS) -o ${CUFILE}.taffo.ll \
 			2> taffo_kern_logs/taffo.log
 	$(LLVM_NVPTX) \
 		-mcpu=sm_86 -o ${PTXFILE_TAFFO} \
@@ -41,8 +89,9 @@ ${PTXFILE_TAFFO}:
 
 .PHONY: run
 run: ${EXECUTABLE} ${EXECUTABLE_TAFFO}
-	./${EXECUTABLE} 2> ${EXECUTABLE}.txt
-	./${EXECUTABLE_TAFFO} 2> ${EXECUTABLE_TAFFO}.txt
+	echo BENCHMARK = $$(basename $$(pwd))
+	ulimit -s unlimited; ./${EXECUTABLE} 2> ${EXECUTABLE}.txt
+	ulimit -s unlimited; ./${EXECUTABLE_TAFFO} 2> ${EXECUTABLE_TAFFO}.txt
 
 .PHONY: validate
 validate:
