4008a912d089b9bb64101037b1661bab340685b7
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2023-03-01 17:32:11 +0000

    Polybench/ACC: Add syrk benchmark.

---
+++ test/polybench-acc-nvcl/syrk/syrk_sh_ann.h
@@ -0,0 +1,9 @@
+#ifndef SYR2K_SH_ANN_H
+#define SYR2K_SH_ANN_H
+
+#define ANN_ALPHA __attribute__((annotate("target('alpha') scalar(range(32412, 32412) final) bufferid('alpha')")))
+#define ANN_BETA __attribute__((annotate("target('beta') scalar(range(2123, 2123) final) bufferid('beta')")))
+#define ANN_A __attribute__((annotate("target('a') scalar(range(0, 1) final) bufferid('a')")))
+#define ANN_C __attribute__((annotate("target('c') scalar(range(0, 11027638) final) bufferid('c')")))
+
+#endif
