ca6f5aeef5782d28819462954c5ee49394762030
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2023-03-07 20:44:26 +0000

    Polybench/ACC: Add doitgen benchmark

---
+++ test/polybench-acc-nvcl/doitgen/doitgen_sh_ann.h
@@ -0,0 +1,8 @@
+#ifndef DOITGEN_SH_ANN_H
+#define DOITGEN_SH_ANN_H
+
+#define ANN_A __attribute__((annotate("scalar(range(0,1100000) final) target('') bufferid('a')")))
+#define ANN_SUM __attribute__((annotate("scalar(range(0,1100000) final) target('') bufferid('sum')")))
+#define ANN_C4 __attribute__((annotate("scalar(range(0,100) final) target('') bufferid('c4')")))
+
+#endif
