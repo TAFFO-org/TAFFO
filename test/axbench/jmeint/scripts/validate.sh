9e331e473b84e8600e95ba2225a39113a6179be8
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2023-04-06 17:41:07 +0200

    AxBench: Use new build system for jmeint

---
+++ test/axbench/jmeint/scripts/validate.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+if [[ -z $FORMAT ]]; then
+  FORMAT='%40s %12s %12s%12s%12s\n'
+fi
+
+
+match_time()
+{
+  regex='^kernel[^0-9]*([^ ]+)'
+  if [[ ( $1 =~ $regex ) ]]; then
+    echo ${BASH_REMATCH[1]}
+  else
+    echo -1
+  fi
+}
+
+match_error()
+{
+  regex="^[^${2:0:1}]*$2[^0-9]*([0-9+-e.]+)"
+  if [[ ( $1 =~ $regex ) ]]; then
+    echo ${BASH_REMATCH[1]}
+  else
+    echo -1
+  fi
+}
+
+benchmark=jmeint
+
+for f in data/input/*.data
+do
+  filename=$(basename "$f")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  
+  mfloat=$(match_time "`cat data/output/${filename}_${benchmark}_time.txt`")
+  mfix=$(match_time "`cat data/output/${filename}_${benchmark}_time-taffo.txt`")
+  error=$(./scripts/qos.py data/output/${filename}_${benchmark}_out.data data/output/${filename}_${benchmark}_out.data.fixp)
+  mabs_error=$(match_error "$error" 'Absolute error')
+  mrel_error=$(match_error "$error" 'Relative error')
+  
+  printf "$FORMAT" "${benchmark}_${filename}" $mfix $mfloat "$mrel_error" "$mabs_error"
+done
