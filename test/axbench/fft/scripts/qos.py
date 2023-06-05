c525f8ecd14283599c1e528e9af5aa5e81ad9b38
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2023-06-05 17:26:57 +0200

    Port axbench QOS scripts to python 3.

--- test/axbench/fft/scripts/qos.py
+++ test/axbench/fft/scripts/qos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import sys
 import math
@@ -14,20 +14,23 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def printUsage():
-  print "Usage: python qos.py <original file> <nn file>"
-  exit(1)
-pass;
+    print("Usage: python qos.py <original file> <nn file>")
+    exit(1)
 
 
-if(len(sys.argv) != 3):
-  printUsage()
+pass
 
-origFilename  = sys.argv[1]
-nnFilename    = sys.argv[2]
 
-origLines     = open(origFilename).readlines()
-nnLines     = open(nnFilename).readlines()
+if (len(sys.argv) != 3):
+    printUsage()
+
+origFilename = sys.argv[1]
+nnFilename = sys.argv[2]
+
+origLines = open(origFilename).readlines()
+nnLines = open(nnFilename).readlines()
 
 
 e = 0.0
@@ -35,40 +38,41 @@ absError = 0.0
 trueAbsError = 0.0
 
 for i in range(len(origLines)):
-  ##import pdb
-  ##pdb.set_trace()
+    # import pdb
+    # pdb.set_trace()
 
-  origLine  = origLines[i].rstrip()
-  nnLine    = nnLines[i].rstrip()
+    origLine = origLines[i].rstrip()
+    nnLine = nnLines[i].rstrip()
 
+    origReal = float(origLine.split(" ")[0])
+    origImag = float(origLine.split(" ")[1])
 
-  origReal  = float(origLine.split(" ")[0])
-  origImag  = float(origLine.split(" ")[1])
+    nnReal = float(nnLine.split(" ")[0])
+    nnImag = float(nnLine.split(" ")[1])
 
-  nnReal    = float(nnLine.split(" ")[0])
-  nnImag    = float(nnLine.split(" ")[1])
+    diffReal = origReal - nnReal
+    diffImag = origImag - nnImag
 
-  diffReal  = origReal - nnReal
-  diffImag  = origImag - nnImag
+    nominator = math.sqrt(diffReal*diffReal + diffImag*diffImag)
+    denominator = math.sqrt(origReal*origReal + origImag*origImag)
 
-  nominator   = math.sqrt(diffReal*diffReal + diffImag*diffImag)
-  denominator = math.sqrt(origReal*origReal + origImag*origImag)
-
-  if(denominator == 0):
-    if (nominator == 0):
-      e = 0.0
+    if (denominator == 0):
+        if (nominator == 0):
+            e = 0.0
+        else:
+            e = 1.0
+    elif (math.isnan(nominator) or (math.isnan(denominator))):
+        e = 1.0
+    elif ((nominator / denominator > 1)):
+        e = 1.0
     else:
-      e = 1.0
-  elif(math.isnan(nominator) or (math.isnan(denominator))):
-    e = 1.0
-  elif ((nominator / denominator > 1)):
-    e = 1.0
-  else:
-    e = nominator / denominator
+        e = nominator / denominator
 
-  absError += e
-  trueAbsError += nominator
-pass;
+    absError += e
+    trueAbsError += nominator
+pass
 
-print bcolors.WARNING + "*** Relative error: %1.8f %%" % (absError/float(len(origLines)) * 100.0) + bcolors.ENDC
-print bcolors.WARNING + "*** Absolute error: %1.8f" % (trueAbsError/float(len(origLines))) + bcolors.ENDC
+print(bcolors.WARNING + "*** Relative error: %1.8f %%" %
+      (absError/float(len(origLines)) * 100.0) + bcolors.ENDC)
+print(bcolors.WARNING + "*** Absolute error: %1.8f" %
+      (trueAbsError/float(len(origLines))) + bcolors.ENDC)
--- test/axbench/kmeans/scripts/qos.py
+++ test/axbench/kmeans/scripts/qos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import sys
 import math
@@ -15,7 +15,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def printUsage():
-  print "Usage: python qos.py <original file> <nn file>"
+  print("Usage: python qos.py <original file> <nn file>")
   exit(1)
 pass;
 
@@ -65,6 +65,6 @@ for i in range(len(origLines)):
   trueAbsError += nominator
 pass;
 
-print bcolors.WARNING + "*** Relative error: %1.8f %%" % (absError/float(len(origLines)) * 100.0) + bcolors.ENDC
-print bcolors.WARNING + "*** Absolute error: %1.8f" % (trueAbsError/float(len(origLines))) + bcolors.ENDC
-print bcolors.WARNING + "*** Maximum value: %1.8f" % (maxDist) + bcolors.ENDC
+print(bcolors.WARNING + "*** Relative error: %1.8f %%" % (absError/float(len(origLines)) * 100.0) + bcolors.ENDC)
+print(bcolors.WARNING + "*** Absolute error: %1.8f" % (trueAbsError/float(len(origLines))) + bcolors.ENDC)
+print(bcolors.WARNING + "*** Maximum value: %1.8f" % (maxDist) + bcolors.ENDC)
