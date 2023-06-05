c822bacd4416c1ce5bc4d2952a0ac196744fe980
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2023-06-05 21:56:17 +0200

    Revert "Cleanup OpenMP benchmarks."
    This reverts commit d113b611f418a00ba40c8486b76ca1144c9e09f4.

---
+++ test/polybench-openmp/medley/floyd-warshall/floyd-warshall.h
@@ -0,0 +1,71 @@
+/**
+ * floyd-warshall.h: This file is part of the PolyBench/C 3.2 test suite.
+ *
+ *
+ * Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
+ * Web address: http://polybench.sourceforge.net
+ */
+#ifndef FLOYD_WARSHALL_H
+# define FLOYD_WARSHALL_H
+
+/* Default to STANDARD_DATASET. */
+# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
+#  define STANDARD_DATASET
+# endif
+
+/* Do not define anything if the user manually defines the size. */
+# ifndef N
+/* Define the possible dataset sizes. */
+#  ifdef MINI_DATASET
+#   define N 32
+#  endif
+
+#  ifdef SMALL_DATASET
+#   define N 128
+#  endif
+
+#  ifdef STANDARD_DATASET /* Default if unspecified. */
+#   define N 1024
+#  endif
+
+#  ifdef LARGE_DATASET
+#   define N 2000
+#  endif
+
+#  ifdef EXTRALARGE_DATASET
+#   define N 4000
+#  endif
+# endif /* !N */
+
+# define _PB_N POLYBENCH_LOOP_BOUND(N,n)
+
+/* Default data type */
+# if !defined(DATA_TYPE_IS_INT) && !defined(DATA_TYPE_IS_FLOAT) && !defined(DATA_TYPE_IS_DOUBLE)
+#  define DATA_TYPE_IS_DOUBLE
+# endif
+
+#ifdef DATA_TYPE_IS_INT
+#  define DATA_TYPE int
+#  define DATA_PRINTF_MODIFIER "%d "
+#endif
+
+#ifdef DATA_TYPE_IS_FLOAT
+#  define DATA_TYPE float
+#  define DATA_PRINTF_MODIFIER "%0.16f "
+#  define SCALAR_VAL(x) x##f
+#  define SQRT_FUN(x) sqrtf(x)
+#  define EXP_FUN(x) expf(x)
+#  define POW_FUN(x,y) powf(x,y)
+# endif
+
+#ifdef DATA_TYPE_IS_DOUBLE
+#  define DATA_TYPE double
+#  define DATA_PRINTF_MODIFIER "%0.16lf "
+#  define SCALAR_VAL(x) x
+#  define SQRT_FUN(x) sqrt(x)
+#  define EXP_FUN(x) exp(x)
+#  define POW_FUN(x,y) pow(x,y)
+# endif
+
+
+#endif /* !FLOYD_WARSHALL */
