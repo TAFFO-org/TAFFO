5b24c5ad16ea620420ee78c502d3210ee715baba
ilgeco <gabriele.magnani@mail.polimi.it>
2023-09-07 16:08:40 +0200

    new range + clangformat

--- test/polybench-cpu/datamining/correlation/correlation.c
+++ test/polybench-cpu/datamining/correlation/correlation.c
@@ -9,10 +9,10 @@
  */
 /* correlation.c: this file is part of PolyBench/C */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /* Include polybench common header. */
 #include <polybench.h>
@@ -22,11 +22,10 @@
 
 
 /* Array initialization. */
-static
-void init_array (int m,
-		 int n,
-		 DATA_TYPE *float_n,
-		 DATA_TYPE POLYBENCH_2D(data,N,M,n,m))
+static void init_array(int m,
+                       int n,
+                       DATA_TYPE *float_n,
+                       DATA_TYPE POLYBENCH_2D(data, N, M, n, m))
 {
   int __attribute((annotate("scalar(range(0, 260) final disabled)"))) i;
   int __attribute((annotate("scalar(range(0, 240) final disabled)"))) j;
@@ -35,16 +34,14 @@ void init_array (int m,
 
   for (i = 0; i < N; i++)
     for (j = 0; j < M; j++)
-      data[i][j] = ((DATA_TYPE)(i*j)/M + i)/N;
-
+      data[i][j] = ((DATA_TYPE)(i * j) / M + i) / N;
 }
 
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
-static
-void print_array(int m,
-		 DATA_TYPE POLYBENCH_2D(corr,M,M,m,m))
+static void print_array(int m,
+                        DATA_TYPE POLYBENCH_2D(corr, M, M, m, m))
 
 {
   int i, j;
@@ -53,8 +50,9 @@ void print_array(int m,
   POLYBENCH_DUMP_BEGIN("corr");
   for (i = 0; i < m; i++)
     for (j = 0; j < m; j++) {
-      if ((i * m + j) % 20 == 0) fprintf (POLYBENCH_DUMP_TARGET, "\n");
-      fprintf (POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, corr[i][j]);
+      if ((i * m + j) % 20 == 0)
+        fprintf(POLYBENCH_DUMP_TARGET, "\n");
+      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, corr[i][j]);
     }
   POLYBENCH_DUMP_END("corr");
   POLYBENCH_DUMP_FINISH;
@@ -63,13 +61,12 @@ void print_array(int m,
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
-static
-void kernel_correlation(int m, int n,
-			DATA_TYPE float_n,
-			DATA_TYPE POLYBENCH_2D(data,N,M,n,m),
-			DATA_TYPE POLYBENCH_2D(corr,M,M,m,m),
-			DATA_TYPE POLYBENCH_1D(mean,M,m),
-			DATA_TYPE POLYBENCH_1D(stddev,M,m))
+static void kernel_correlation(int m, int n,
+                               DATA_TYPE float_n,
+                               DATA_TYPE POLYBENCH_2D(data, N, M, n, m),
+                               DATA_TYPE POLYBENCH_2D(corr, M, M, m, m),
+                               DATA_TYPE POLYBENCH_1D(mean, M, m),
+                               DATA_TYPE POLYBENCH_1D(stddev, M, m))
 {
   int i, j, k;
 
@@ -77,79 +74,73 @@ void kernel_correlation(int m, int n,
 
 
 #pragma scop
-  for (j = 0; j < _PB_M; j++)
-    {
-      mean[j] = SCALAR_VAL(0.0);
-      for (i = 0; i < _PB_N; i++)
-	mean[j] += data[i][j];
-      mean[j] /= float_n;
-    }
+  for (j = 0; j < _PB_M; j++) {
+    mean[j] = SCALAR_VAL(0.0);
+    for (i = 0; i < _PB_N; i++)
+      mean[j] += data[i][j];
+    mean[j] /= float_n;
+  }
 
 
-   for (j = 0; j < _PB_M; j++)
-    {
-      stddev[j] = SCALAR_VAL(0.0);
-      for (i = 0; i < _PB_N; i++)
-        stddev[j] += (data[i][j] - mean[j]) * (data[i][j] - mean[j]);
-      stddev[j] /= float_n;
-      stddev[j] = SQRT_FUN(stddev[j]);
-      /* The following in an inelegant but usual way to handle
-         near-zero std. dev. values, which below would cause a zero-
-         divide. */
-      stddev[j] = stddev[j] <= eps ? SCALAR_VAL(1.0) : stddev[j];
-    }
+  for (j = 0; j < _PB_M; j++) {
+    stddev[j] = SCALAR_VAL(0.0);
+    for (i = 0; i < _PB_N; i++)
+      stddev[j] += (data[i][j] - mean[j]) * (data[i][j] - mean[j]);
+    stddev[j] /= float_n;
+    stddev[j] = SQRT_FUN(stddev[j]);
+    /* The following in an inelegant but usual way to handle
+       near-zero std. dev. values, which below would cause a zero-
+       divide. */
+    stddev[j] = stddev[j] <= eps ? SCALAR_VAL(1.0) : stddev[j];
+  }
 
   /* Center and reduce the column vectors. */
   for (i = 0; i < _PB_N; i++)
-    for (j = 0; j < _PB_M; j++)
-      {
-        data[i][j] -= mean[j];
-        data[i][j] /= SQRT_FUN(float_n) * stddev[j];
-      }
+    for (j = 0; j < _PB_M; j++) {
+      data[i][j] -= mean[j];
+      data[i][j] /= SQRT_FUN(float_n) * stddev[j];
+    }
 
   /* Calculate the m * m correlation matrix. */
-  for (i = 0; i < _PB_M-1; i++)
-    {
-      corr[i][i] = SCALAR_VAL(1.0);
-      for (j = i+1; j < _PB_M; j++)
-        {
-          corr[i][j] = SCALAR_VAL(0.0);
-          for (k = 0; k < _PB_N; k++)
-            corr[i][j] += (data[k][i] * data[k][j]);
-          corr[j][i] = corr[i][j];
-        }
+  for (i = 0; i < _PB_M - 1; i++) {
+    corr[i][i] = SCALAR_VAL(1.0);
+    for (j = i + 1; j < _PB_M; j++) {
+      corr[i][j] = SCALAR_VAL(0.0);
+      for (k = 0; k < _PB_N; k++)
+        corr[i][j] += (data[k][i] * data[k][j]);
+      corr[j][i] = corr[i][j];
     }
-  corr[_PB_M-1][_PB_M-1] = SCALAR_VAL(1.0);
+  }
+  corr[_PB_M - 1][_PB_M - 1] = SCALAR_VAL(1.0);
 #pragma endscop
-
 }
 
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   /* Retrieve problem size. */
   int n = N;
   int m = M;
 
   /* Variable declaration/allocation. */
-  DATA_TYPE __attribute((annotate("scalar(range(1, 3000))"))) float_n;
-  POLYBENCH_2D_ARRAY_DECL(data,DATA_TYPE __attribute((annotate("scalar(range(-512, 512) final)"))),N,M,n,m);
-  POLYBENCH_2D_ARRAY_DECL(corr,DATA_TYPE __attribute((annotate("target('corr') scalar()"))),M,M,m,m);
-  POLYBENCH_1D_ARRAY_DECL(mean,DATA_TYPE __attribute((annotate("scalar()"))),M,m);
-  POLYBENCH_1D_ARRAY_DECL(stddev,DATA_TYPE __attribute((annotate("scalar(range(1,4096) final)"))),M,m);
+  DATA_TYPE __attribute((annotate("scalar(range(260, 260) final)"))) float_n;
+  POLYBENCH_2D_ARRAY_DECL(data, DATA_TYPE __attribute((annotate("scalar(range(-2, 2))"))), N, M, n, m);
+  POLYBENCH_2D_ARRAY_DECL(corr, DATA_TYPE __attribute((annotate("target('corr') scalar(range(-2,2))"))), M, M, m, m);
+  POLYBENCH_1D_ARRAY_DECL(mean, DATA_TYPE __attribute((annotate("scalar(range(-1,259))"))), M, m);
+  POLYBENCH_1D_ARRAY_DECL(stddev, DATA_TYPE __attribute((annotate("scalar(range(-1,86) final)"))), M, m);
 
   /* Initialize array(s). */
-  init_array (m, n, &float_n, POLYBENCH_ARRAY(data));
+  init_array(m, n, &float_n, POLYBENCH_ARRAY(data));
 
   /* Start timer. */
   polybench_start_instruments;
 
   /* Run kernel. */
-  kernel_correlation (m, n, float_n,
-		      POLYBENCH_ARRAY(data),
-		      POLYBENCH_ARRAY(corr),
-		      POLYBENCH_ARRAY(mean),
-		      POLYBENCH_ARRAY(stddev));
+  kernel_correlation(m, n, float_n,
+                     POLYBENCH_ARRAY(data),
+                     POLYBENCH_ARRAY(corr),
+                     POLYBENCH_ARRAY(mean),
+                     POLYBENCH_ARRAY(stddev));
 
   /* Stop and print timer. */
   polybench_stop_instruments;
